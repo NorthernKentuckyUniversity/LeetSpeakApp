3f392508786ccbe7f88e6d6b5776b21ed047f571
Brian Konzman <bkonzman@gmail.com>
2016-07-19 22:59:44 -0400

    initial commit

--- LeetSpeak/ViewController.swift
+++ LeetSpeak/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var InputText: UITextField!
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+
+    @IBAction func ButtonPressed(sender: UIButton) {
+        OutputLabel.text = convertStringFromEnglishToLeet(InputText.text!)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func convertStringFromEnglishToLeet(input : String) -> String {
+        return input.lowercaseString
+            .stringByReplacingOccurrencesOfString("l", withString: "1")
+            .stringByReplacingOccurrencesOfString("z", withString: "2")
+            .stringByReplacingOccurrencesOfString("e", withString: "3")
+            .stringByReplacingOccurrencesOfString("a", withString: "4")
+            .stringByReplacingOccurrencesOfString("s", withString: "5")
+            .stringByReplacingOccurrencesOfString("a", withString: "6")
+            .stringByReplacingOccurrencesOfString("t", withString: "7")
+            .stringByReplacingOccurrencesOfString("b", withString: "8")
+            .stringByReplacingOccurrencesOfString("o", withString: "0")
+    }
+    
 }
 
--- LeetSpeakTests/LeetSpeakTests.swift
+++ LeetSpeakTests/LeetSpeakTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class LeetSpeakTests: XCTestCase {
     
+    let Controller = ViewController()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,16 +23,27 @@ class LeetSpeakTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testConvertLowerString() {
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("l"), "1")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("z"), "2")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("e"), "3")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("a"), "4")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("s"), "5")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("g"), "6")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("t"), "7")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("b"), "8")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("o"), "0")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+    func testConvertUpperString() {
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("L"), "1")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("Z"), "2")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("E"), "3")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("A"), "4")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("S"), "5")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("G"), "6")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("T"), "7")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("B"), "8")
+        XCTAssertEqual(Controller.convertStringFromEnglishToLeet("O"), "0")
+    }    
 }
